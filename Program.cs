0459bd7361d13b2a0927e108bcaf7a1478a56dab
Mariam <mariam.khachiyan@gmail.com>
2023-08-04 06:46:52 +0400

    Homework 3

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿﻿// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+/* Console.WriteLine("Enter five-digit number: ");
+string num = Console.ReadLine();
+int length = num.Length;
+
+if (length == 5)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine("Polindrome number");
+    }
+    else
+    {
+        Console.WriteLine("Non polindromic number");
+    }
+
+
+}
+else
+{
+    Console.WriteLine("The number isn't five-digit!");
+}
+
+ */
+
+// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+/* 
+Console.WriteLine("Enter number : ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number : ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number : ");
+int X3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number : ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number : ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number : ");
+int Y3 = Convert.ToInt32(Console.ReadLine());
+
+
+double D = Math.Sqrt(Math.Pow((X1 - Y1),2) + Math.Pow((X2 - Y2),2) + Math.Pow((X3- Y3),2));
+Console.WriteLine($"Distance between points {D}"); */
+
+
+
+
+// Напишите программу, которая принимает на 
+//вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+/* Console.Write("Enter number: ");
+int  num = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=num; i++)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+} */
+
+
+
